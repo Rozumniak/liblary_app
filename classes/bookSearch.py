b6f59c0338f4708a4663bbf5f6cbdc0ffa96dc21
Ruslan <rusroz77@gmail.com>
2023-12-15 14:56:51 +0200

    venv delete

---
+++ classes/bookSearch.py
@@ -0,0 +1,197 @@
+from PyQt6 import QtWidgets, QtCore
+from design.booksearch_design import Ui_Booksearch
+
+
+from database import get_unique_genres, search_by_genre, search_by_title, search_by_author
+
+class Booksearch(QtWidgets.QWidget, QtCore.QObject, Ui_Booksearch):
+    visitors_clicked = QtCore.pyqtSignal()
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.visitors_button.clicked.connect(self.visitorsClicked)
+        self.books_button.setCheckable(True)
+        self.books_button.setChecked(True)
+        self.title_radio.setChecked(True)
+        genres = get_unique_genres()
+        self.genre_combo.addItems(genres)
+        self.genre_combo.currentIndexChanged.connect(self.genre_search)
+        self.search_button.clicked.connect(self.search_button_clicked)
+        self.add_book_button.clicked.connect(self.addBookClicked)
+
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key.Key_Enter or event.key() == QtCore.Qt.Key.Key_Return:
+            self.search_button_clicked()
+
+    def addBookClicked(self):
+        from classes.addNewBook import AddNewBook
+        self.addBook = AddNewBook()
+        self.addBook.show()
+
+    def genre_search(self):
+        self.scrollAreaWidgetContents.deleteLater()
+
+        selected_genre = self.genre_combo.currentText()
+        books = search_by_genre(self.genre_combo.currentText())
+
+
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 929, 499))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.labels_layout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+
+        for i in books:
+            book_id, author, title, genre, available = i[0:]
+
+            self.label = QtWidgets.QLabel()
+            self.label.setStyleSheet("background-color: rgb(164, 201, 255);"
+                                     "border-radius: 10px;"
+                                     "padding: 0px;"
+                                     "font: 14pt \"Arial\";")
+            self.label.setFixedHeight(60)
+            self.label.setFixedWidth(self.scrollArea.width() - 40)
+
+            self.label_layout = QtWidgets.QHBoxLayout(self.label)
+
+            label_text = QtWidgets.QLabel(f"Автор: {author}| Назва: {title}| Жанр: {genre}|\n Наявність: {available}")
+            self.label_layout.addWidget(label_text)
+
+            self.give_book = QtWidgets.QPushButton(f"Видати")
+            self.give_book.setGeometry(QtCore.QRect(750, 70, 111, 41))
+            self.give_book.setStyleSheet("border-radius: 10px;\n"
+                                         "border-color: rgb(0, 0, 0);\n"
+                                         "\n"
+                                         "background-color: rgb(255, 242, 239);\n"
+                                         "font: 14pt \"Arial\";\n"
+                                         "")
+            self.give_book.setFixedHeight(40)
+            self.give_book.setFixedWidth(150)
+            self.give_book.clicked.connect(lambda _, book_id=i: self.giveBookClicked(book_id))
+            self.label_layout.addWidget(self.give_book)
+            self.labels_layout.addWidget(self.label)
+            self.labels_layout.setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetFixedSize)
+    def search_button_clicked(self):
+        if self.author_radio.isChecked():
+            self.search_by_author()
+        elif self.title_radio.isChecked():
+            self.search_by_title()
+
+    def visitorsClicked(self):
+        self.visitors_clicked.emit()
+
+
+    def giveBookClicked(self, book_id):
+        from classes.giveBook import GiveBook
+        self.givebook = GiveBook(book_id)
+        self.givebook.show()
+
+    def search_by_title(self):
+        self.scrollAreaWidgetContents.deleteLater()
+        self.title = self.search_input.text()
+        books = search_by_title(self.title)
+
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 929, 499))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.labels_layout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+        if books:
+            for i in books:
+                book_id, author, title, genre, available = i[0:]
+
+                self.label = QtWidgets.QLabel()
+                self.label.setStyleSheet("background-color: rgb(164, 201, 255);"
+                                         "border-radius: 10px;"
+                                         "padding: 0px;"
+                                         "font: 14pt \"Arial\";")
+                self.label.setFixedHeight(60)
+                self.label.setFixedWidth(self.scrollArea.width() - 40)
+
+                self.label_layout = QtWidgets.QHBoxLayout(self.label)
+
+                label_text = QtWidgets.QLabel(f"Автор: {author}| Назва: {title}| Жанр: {genre}|\n Наявність: {available}")
+                self.label_layout.addWidget(label_text)
+
+                self.give_book = QtWidgets.QPushButton(f"Видати")
+                self.give_book.setGeometry(QtCore.QRect(750, 70, 111, 41))
+                self.give_book.setStyleSheet("border-radius: 10px;\n"
+                                             "border-color: rgb(0, 0, 0);\n"
+                                             "\n"
+                                             "background-color: rgb(255, 242, 239);\n"
+                                             "font: 14pt \"Arial\";\n"
+                                             "")
+                self.give_book.setFixedHeight(40)
+                self.give_book.setFixedWidth(150)
+                self.give_book.clicked.connect(lambda _, book_id=i: self.giveBookClicked(book_id))
+                self.label_layout.addWidget(self.give_book)
+                self.labels_layout.addWidget(self.label)
+                self.labels_layout.setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetFixedSize)
+        else:
+            label_text = QtWidgets.QLabel(f"Таку книгу не знайдено")
+            label_text.setStyleSheet("background-color: red;"
+                                         "border-radius: 10px;"
+                                         "padding: 0px;"
+                                         "font: 14pt \"Arial\";")
+            label_text.setFixedHeight(40)
+            label_text.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+            self.labels_layout.addWidget(label_text)
+            self.labels_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
+
+
+    def search_by_author(self):
+        self.scrollAreaWidgetContents.deleteLater()
+        self.author = self.search_input.text()
+        books = search_by_author(self.author)
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 929, 499))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+
+        self.labels_layout = QtWidgets.QVBoxLayout(self.scrollAreaWidgetContents)
+
+
+        if books:
+            for i in books:
+                book_id, author, title, genre, available = i[0:]
+
+                self.label = QtWidgets.QLabel()
+                self.label.setStyleSheet("background-color: rgb(164, 201, 255);"
+                                    "border-radius: 10px;"
+                                    "padding: 0px;"
+                                    "font: 14pt \"Arial\";")
+                self.label.setFixedHeight(60)
+                self.label.setFixedWidth(self.scrollArea.width()-40)
+
+                self.label_layout = QtWidgets.QHBoxLayout(self.label)
+
+                label_text = QtWidgets.QLabel(f"Автор: {author}| Назва: {title}| Жанр: {genre}|\n Наявність: {available}")
+                self.label_layout.addWidget(label_text)
+
+                self.give_book = QtWidgets.QPushButton(f"Видати")
+                self.give_book.setGeometry(QtCore.QRect(750, 70, 111, 41))
+                self.give_book.setStyleSheet("border-radius: 10px;\n"
+                                             "border-color: rgb(0, 0, 0);\n"
+                                             "\n"
+                                             "background-color: rgb(255, 242, 239);\n"
+                                             "font: 14pt \"Arial\";\n"
+                                             "")
+                self.give_book.setFixedHeight(40)
+                self.give_book.setFixedWidth(150)
+                self.give_book.clicked.connect(lambda _, book_id=i: self.giveBookClicked(book_id))
+                self.label_layout.addWidget(self.give_book)
+                self.labels_layout.addWidget(self.label)
+                self.labels_layout.setSizeConstraint(QtWidgets.QLayout.SizeConstraint.SetFixedSize)
+        else:
+            label_text = QtWidgets.QLabel(f"Таку книгу не знайдено")
+            label_text.setStyleSheet("background-color: red;"
+                                         "border-radius: 10px;"
+                                         "padding: 0px;"
+                                         "font: 14pt \"Arial\";")
+            label_text.setFixedHeight(40)
+            label_text.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+            self.labels_layout.addWidget(label_text)
+            self.labels_layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignTop)
---
+++ classes/loginWindow.py
@@ -0,0 +1,108 @@
+
+from PyQt6 import QtWidgets, QtCore
+from design.login_design import Ui_Login
+from database import search_worker
+
+
+
+class LoginWindow(QtWidgets.QWidget, QtCore.QObject, Ui_Login):
+    login_successful = QtCore.pyqtSignal()
+
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.setMinimumSize(380, 600)
+        self.pushButton.clicked.connect(self.buttonClicked)
+        self.login_issue.hide()
+        self.password_issue.hide()
+        self.login_input.setFocus()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key.Key_Enter or event.key() == QtCore.Qt.Key.Key_Return:
+            self.buttonClicked()
+
+    def buttonClicked(self):
+        from classes.newWorker import NewWorker
+        login = self.login_input.text().strip()
+        password = self.password_input.text().strip()
+        worker = search_worker(login, password)
+        has_errors = False
+
+        if login == '':
+            self.login_issue.show()
+            self.login_issue.setText("Поле пусте")
+            self.login_input.setStyleSheet("border-radius: 10px;\n"
+                                    "font: 12pt \"Arial\";\n"
+                                    "border: 2px solid red;\n"
+                                    "background-color: white;\n"
+                                    "")
+            has_errors = True
+        else:
+            self.login_issue.hide()
+            self.login_input.setStyleSheet("border-radius: 10px;\n"
+                                     "font: 12pt \"Arial\";\n"
+                                     "border: 2px solid rgb(164, 201, 255);\n"
+                                     "background-color: white;\n"
+                                     "")
+
+        if password == '':
+            self.password_issue.show()
+            self.password_issue.setText("Поле пусте")
+            self.password_input.setStyleSheet("border-radius: 10px;\n"
+                                    "font: 12pt \"Arial\";\n"
+                                    "border: 2px solid red;\n"
+                                    "background-color: white;\n"
+                                    "")
+            self.password_issue.move(190, self.password_issue.y())
+            has_errors = True
+        else:
+            self.password_issue.hide()
+            self.password_input.setStyleSheet("border-radius: 10px;\n"
+                                     "font: 12pt \"Arial\";\n"
+                                     "border: 2px solid rgb(164, 201, 255);\n"
+                                     "background-color: white;\n"
+                                     "")
+
+        if has_errors:
+            self.update()
+        else:
+            if worker == False:
+                self.login_input.setStyleSheet("border-radius: 10px;\n"
+                                               "font: 12pt \"Arial\";\n"
+                                               "border: 2px solid red;\n"
+                                               "background-color: white;\n"
+                                               "")
+                self.password_issue.show()
+                self.password_issue.setText("Логін чи пароль введено невірно")
+                self.password_issue.setFixedWidth(self.password_issue.width() + 45)
+                self.password_issue.move(80, self.password_issue.y())
+                self.password_input.setStyleSheet("border-radius: 10px;\n"
+                                                  "font: 12pt \"Arial\";\n"
+                                                  "border: 2px solid red;\n"
+                                                  "background-color: white;\n"
+                                                  "")
+
+            elif worker[0][3] == 'admin' and worker[0][4] == 'admin':
+
+
+                self.newWorker = NewWorker()
+                self.newWorker.show()
+            elif worker:
+                self.login_successful.emit()
+                self.hide()
+
+                # booksearch = Booksearch()
+                # main.widget.addWidget(booksearch)
+                #
+                #
+                # main.widget.setMinimumWidth(1107)
+                # main.widget.setMinimumHeight(719)
+                #
+                # screen_width = QtGui.QGuiApplication.primaryScreen().geometry().width()
+                # screen_height = QtGui.QGuiApplication.primaryScreen().geometry().height()
+                # widget_width = main.widget.width()
+                # widget_height = main.widget.height()
+                #
+                # main.widget.move(int((screen_width - widget_width) / 2), int((screen_height - widget_height) / 2))
+                # main.widget.setCurrentIndex(main.widget.currentIndex() + 1)
+                # booksearch.show()
